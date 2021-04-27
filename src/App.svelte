267dfccf4ef554328d24556fbf0ac83b3310a070
Kuan <kuan@hey.com>
2021-04-27 14:47:07 +0800

    Social button

--- src/App.svelte
+++ src/App.svelte
@@ -15,6 +15,15 @@
     <Link to="/">Home</Link>
     <Link to="about">About</Link>
     <Link to="walk/pasar-besar">Walk</Link>
+    <!-- svelte-ignore a11y-missing-content -->
+    <a href="https://www.facebook.com/kotakitasabah" class="fa fa-facebook" />
+    <!-- svelte-ignore a11y-missing-content -->
+    <a href="https://twitter.com/kotaKitaSabah" class="fa fa-twitter" />
+    <!-- svelte-ignore a11y-missing-content -->
+    <a
+      href="https://www.instagram.com/kotakitasabah/"
+      class="fa fa-instagram"
+    />
   </nav>
   <div>
     <Route path="/">
@@ -43,6 +52,37 @@
     margin: 0 auto;
   }
 
+  /* Style all font awesome icons */
+  .fa {
+    padding: 20px;
+    font-size: 30px;
+    width: 30px;
+    text-align: center;
+    text-decoration: none;
+  }
+
+  /* Add a hover effect if you want */
+  .fa:hover {
+    opacity: 0.7;
+  }
+
+  /* Set a specific color for each brand */
+
+  /* Facebook */
+  .fa-facebook {
+    background: #3b5998;
+    color: white;
+  }
+
+  /* Twitter */
+  .fa-twitter {
+    background: #55acee;
+    color: white;
+  }
+  .fa-instagram {
+    background: #125688;
+    color: white;
+  }
   @media (min-width: 640px) {
     main {
       max-width: none;
