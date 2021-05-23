92ac2a8740d977d751f19b0bee4c18c02fc4af1a
Kuan <kuan@hey.com>
2021-05-23 20:07:35 +0800

    persist route by day

--- src/route_store.js
+++ src/route_store.js
@@ -1,4 +1,4 @@
-import { writable } from 'svelte/store';
+import { readable, writable } from 'svelte/store';
 
 let visitingPlaceId;
 export const visitingPlace = writable({});
@@ -20,20 +20,50 @@ export const visitPlace = async (placeId) => {
   }
 };
 
-let routes = [];
+const msOfADay = 1000 * 60 * 60 * 24;
+const getTodayMs = () => Math.floor(Date.now() / msOfADay);
+const keyForRoute = (dayMs) => `r_${dayMs}`;
+
+const routeGetter = () => {
+  let routes = JSON.parse(
+    window.localStorage.getItem(keyForRoute(getTodayMs())) ?? '[]'
+  );
+  return () => routes;
+};
+const getCurrentRoute = routeGetter();
+
+let resetRoute;
+export const routes = readable([], (set) => {
+  resetRoute = set;
+
+  resetRoute(getCurrentRoute());
+});
 
 export const checkedIn = writable(false);
 const determineCheckedInStatus = () => {
+  let routes = getCurrentRoute();
   if (routes.length > 0) checkedIn.set(routes.slice(-1)[0] == visitingPlaceId);
   else checkedIn.set(false);
 };
 
 export const checkIn = () => {
+  let routes = getCurrentRoute();
   routes.push(visitingPlaceId);
+  try {
+    window.localStorage.setItem(
+      keyForRoute(getTodayMs()),
+      JSON.stringify(routes)
+    );
+  } catch (error) {
+    console.error(error);
+  }
+
   determineCheckedInStatus();
 };
 
 export const getTodayRoutes = async () => {
+  let routes = getCurrentRoute();
+
   console.log('routes :>> ', routes);
   return routes;
 };
