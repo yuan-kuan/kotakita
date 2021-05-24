473863d12165e2d53a1bc60dcbedcdba75ce66c4
Kuan <kuan@hey.com>
2021-05-24 19:23:56 +0800

    Profile update modal to call anytime small extra info in setting

--- src/Profile.svelte
+++ src/Profile.svelte
@@ -1,15 +1,20 @@
 <script>
-  import {
-    tryAdminAccess,
-    userProfile,
-    forgetUser,
-    updateInfo,
-  } from './user_store';
+  import ProfileUpdater from './ProfileUpdater.svelte';
+  import { tryAdminAccess, userProfile, forgetUser } from './user_store';
 
-  let name = $userProfile.name;
-  let age = $userProfile.age;
-  let gender = $userProfile.gender;
-  let oku = $userProfile.oku;
+  $: name = $userProfile.name ?? 'New User';
+
+  let otherInfo = '';
+  $: {
+    otherInfo = '';
+    if (
+      $userProfile.gender &&
+      ($userProfile.gender != '' || $userProfile.gender != 'private')
+    )
+      otherInfo += `${$userProfile.gender}`;
+    if ($userProfile.age) otherInfo += `(${$userProfile.age})`;
+    if ($userProfile.oku) otherInfo += ', has disability';
+  }
 
   let isShowingDropDown = false;
   const closeDropdown = () => (isShowingDropDown = false);
@@ -19,9 +24,7 @@
     isShowingInfoUpdate = true;
     closeDropdown();
   };
-
-  const saveProfile = () => {
-    updateInfo(name, age, gender, oku);
+  const closingInfoUpdate = () => {
     isShowingInfoUpdate = false;
   };
 </script>
@@ -47,6 +50,7 @@
         <!-- svelte-ignore a11y-label-has-associated-control -->
         <label>Logged in as:</label>
         <strong>{name}</strong>
+        <p class="is-size-7 is-italic">{otherInfo}</p>
       </div>
       <!-- svelte-ignore a11y-invalid-attribute -->
       <a href="#" class="dropdown-item" on:click={showInfoUpdate}>
@@ -54,71 +58,27 @@
       </a>
       <hr class="dropdown-divider" />
       <!-- svelte-ignore a11y-invalid-attribute -->
-      <a href="#" class="dropdown-item" on:click={forgetUser}> Forget Me </a>
+      <a
+        href="#"
+        class="dropdown-item"
+        on:click={forgetUser}
+        on:click={closeDropdown}
+      >
+        Forget Me
+      </a>
       <!-- svelte-ignore a11y-invalid-attribute -->
-      <a href="#" class="dropdown-item" on:click={tryAdminAccess}>
+      <a
+        href="#"
+        class="dropdown-item"
+        on:click={tryAdminAccess}
+        on:click={closeDropdown}
+      >
         Admin Access
       </a>
     </div>
   </div>
 </div>
 
-<div class="modal" class:is-active={isShowingInfoUpdate}>
-  <div
-    class="modal-background"
-    on:click={() => (isShowingInfoUpdate = false)}
-  />
-  <div class="modal-content box">
-    <div class="field">
-      <!-- svelte-ignore a11y-label-has-associated-control -->
-      <label class="label">Name</label>
-      <div class="control">
-        <input
-          class="input"
-          type="text"
-          placeholder="Any Name"
-          bind:value={name}
-        />
-      </div>
-    </div>
-
-    <div class="field">
-      <!-- svelte-ignore a11y-label-has-associated-control -->
-      <label class="label">Age</label>
-      <div class="control">
-        <input
-          class="input"
-          type="number"
-          placeholder="Your age"
-          bind:value={age}
-        />
-      </div>
-    </div>
-
-    <div class="select mt-3">
-      <select bind:value={gender}>
-        <option value="">Gender</option>
-        <option value="Female">Female</option>
-        <option value="Male">Male</option>
-        <option value="Private">Private</option>
-      </select>
-    </div>
-
-    <label class="checkbox mt-3">
-      <input type="checkbox" bind:checked={oku} />
-      Do you identify as a person with a disability?
-    </label>
-
-    <div class="level mt-4">
-      <button class="button is-success" on:click={saveProfile}>Save</button>
-      <button
-        class="button is-warning"
-        on:click={() => (isShowingInfoUpdate = false)}>Cancel</button
-      >
-    </div>
-  </div>
-  <button
-    class="modal-close is-large"
-    on:click={() => (isShowingInfoUpdate = false)}
-  />
-</div>
+{#if isShowingInfoUpdate}
+  <ProfileUpdater on:click={closingInfoUpdate} />
+{/if}
---
+++ src/ProfileUpdater.svelte
@@ -0,0 +1,71 @@
+<script>
+  import { userProfile, updateInfo } from './user_store';
+
+  let name = $userProfile.name;
+  let age = $userProfile.age;
+  let gender = $userProfile.gender;
+  let oku = $userProfile.oku;
+
+  let isShowingInfoUpdate = false;
+  const showInfoUpdate = () => {
+    isShowingInfoUpdate = true;
+    closeDropdown();
+  };
+
+  const saveProfile = () => {
+    updateInfo(name, age, gender, oku);
+  };
+</script>
+
+<div class="modal is-active">
+  <div class="modal-background" on:click />
+  <div class="modal-content box">
+    <div class="field">
+      <!-- svelte-ignore a11y-label-has-associated-control -->
+      <label class="label">Name</label>
+      <div class="control">
+        <input
+          class="input"
+          type="text"
+          placeholder="Any Name"
+          bind:value={name}
+        />
+      </div>
+    </div>
+
+    <div class="field">
+      <!-- svelte-ignore a11y-label-has-associated-control -->
+      <label class="label">Age</label>
+      <div class="control">
+        <input
+          class="input"
+          type="number"
+          placeholder="Your age"
+          bind:value={age}
+        />
+      </div>
+    </div>
+
+    <div class="select mt-3">
+      <select bind:value={gender}>
+        <option value="">Gender</option>
+        <option value="Female">Female</option>
+        <option value="Male">Male</option>
+        <option value="Private">Private</option>
+      </select>
+    </div>
+
+    <label class="checkbox mt-3">
+      <input type="checkbox" bind:checked={oku} />
+      Do you identify as a person with a disability?
+    </label>
+
+    <div class="level mt-4">
+      <button class="button is-success" on:click={saveProfile} on:click
+        >Save</button
+      >
+      <button class="button is-warning" on:click>Cancel</button>
+    </div>
+  </div>
+  <button class="modal-close is-large" on:click />
+</div>
