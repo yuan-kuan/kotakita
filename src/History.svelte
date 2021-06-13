f0e8888862be378ab24587804c665ab7fd2b8c59
Kuan <kuan@hey.com>
2021-06-13 19:51:22 +0800

    rating to store

--- src/History.svelte
+++ src/History.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { getTodayRoutes } from './route_store';
+  import { getTodayRoutes, rateTheRoute } from './route_store';
 
   const prettyDateTime = (ms) => {
     const d = new Date(ms);
@@ -15,6 +15,11 @@
     console.log(d.toLocaleDateString('en-US', options)); // Saturday, September 17, 2016
     return d.toLocaleDateString('en-US', options);
   };
+
+  const rate = (rating) => {
+    let inc = rating.rating ?? 0;
+    rateTheRoute(rating.key, inc++);
+  };
 </script>
 
 <section class="section has-background-primary ">
@@ -23,23 +28,31 @@
 
 {#await getTodayRoutes()}
   <p>loading...</p>
-{:then routes}
+{:then routeAndRatings}
   <div class="box">
     <ul class="steps is-vertical is-small">
-      {#each routes as route}
+      {#each routeAndRatings as rnr}
         <li class="steps-segment">
           <span href="#" class="steps-marker" />
           <div class="steps-content pb-5">
-            <p class="is-size-4">{route[0]}</p>
+            <p class="is-size-4">{rnr.route[0]}</p>
             <p class="is-size-7 has-text-weight-light">
-              {prettyDateTime(route[1])}
+              {prettyDateTime(rnr.route[1])}
             </p>
-            <button class="button pt-4 is-link is-inverted">
-              <span class="icon">
-                <i class="fas fa-star" />
-              </span>
-              <span>View or edit rating</span>
-            </button>
+            {#if rnr.rating}
+              <p class="is-size-7 has-text-weight-light">
+                {rnr.rating.rating}
+              </p>
+              <button
+                class="button pt-4 is-link is-inverted"
+                on:click={() => rate(rnr.rating)}
+              >
+                <span class="icon">
+                  <i class="fas fa-star" />
+                </span>
+                <span>View or edit rating</span>
+              </button>
+            {/if}
           </div>
         </li>
       {/each}
--- src/route_store.js
+++ src/route_store.js
@@ -65,12 +65,70 @@ export const checkIn = () => {
     console.error(error);
   }
 
+  if (routes.length > 1) {
+    const lastTwo = routes.slice(-2);
+    console.log('lastTwo :>> ', lastTwo);
+    createRatingForRoute(lastTwo[0], lastTwo[1]);
+  }
+
   determineCheckedInStatus();
 };
 
 export const getTodayRoutes = async () => {
-  let routes = getCurrentRoute();
+  const routeAndRating = [];
+  const routes = getCurrentRoute();
+  for (let i = 0; i < routes.length - 1; i++) {
+    const from = routes[i];
+    const to = routes[i + 1];
+    const ratingkey = getKeyForRating(from, to);
+    const rating = getRating(ratingkey);
 
-  console.log('routes :>> ', routes);
-  return routes;
+    routeAndRating.push({
+      route: from,
+      rating: { key: ratingkey, rating: rating },
+    });
+  }
+  routeAndRating.push({ route: routes.slice(-1)[0], rating: null });
+
+  return routeAndRating;
+};
+
+const getKeyForRating = (from, to) => {
+  // eslint-disable-next-line no-unused-vars
+  const [fromPlace, _] = from;
+  const [toPlace, toTime] = to;
+
+  let key = 'ra_';
+  key += fromPlace;
+  key += '_';
+  key += toPlace;
+  key += '_';
+  key += toTime.toString();
+
+  return key;
+};
+
+export const createRatingForRoute = (from, to) => {
+  const key = getKeyForRating(from, to);
+  console.log('key :>> ', key);
+
+  try {
+    window.localStorage.setItem(key, JSON.stringify([]));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getRating = (ratingKey) =>
+  JSON.parse(window.localStorage.getItem(ratingKey) ?? '[]');
+
+export const rateTheRoute = (ratingKey, value) => {
+  let rating = getRating(ratingKey);
+  rating.push(value);
+
+  try {
+    window.localStorage.setItem(ratingKey, JSON.stringify(rating));
+  } catch (error) {
+    console.error(error);
+  }
 };
