9979136a602c5de9f93e9d082e88d4fa94eeff79
Kuan <kuan@hey.com>
2021-06-16 19:45:57 +0800

    scale buttons, route colors

--- src/Rating.svelte
+++ src/Rating.svelte
@@ -4,32 +4,42 @@
 
 <div class="modal is-active">
   <div class="modal-background" on:click />
-  <div class="modal-content">
-    <div class="box">
-      <ul class="steps is-vertical is-small">
-        <li class="steps-segment">
-          <span href="#" class="steps-marker" />
-          <div class="steps-content pb-5">
-            <p class="is-size-4">Gaya Street</p>
-            <p class="is-size-7 has-text-weight-light">some dates</p>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">Rate and Review</p>
+      <button class="delete" aria-label="close" on:click />
+    </header>
+
+    <div class="modal-card-body">
+      <p class="has-text-light">You are rating:</p>
+      <ul class="steps is-vertical is-small is-short has-text-light">
+        <li class="steps-segment is-active ">
+          <span href="#" class="steps-marker is-light" />
+          <div class="steps-content pb-4">
+            <p class="is-size-5">Gaya Street</p>
           </div>
         </li>
         <li class="steps-segment">
-          <span href="#" class="steps-marker" />
-          <div class="steps-content pb-5">
-            <p class="is-size-4">City park</p>
-            <p class="is-size-7 has-text-weight-light">some dates</p>
+          <span href="#" class="steps-marker is-light" />
+          <div class="steps-content pb-4">
+            <p class="is-size-5">City park</p>
           </div>
         </li>
       </ul>
 
-      <div class="buttons is-centered">
-        {#each [1, 2, 3, 4, 5] as n}
-          <button class="button">{n}</button>
+      <!-- <div class="block">
+       {#each [1, 2, 3, 4, 5] as n}
+          <div class="rate-button">{n}</div>
         {/each}
         <br />
         {#each [6, 7, 8, 9, 10] as n}
-          <button class="button">{n}</button>
+          <div class="rate-button">{n}</div>
+        {/each}
+      </div> -->
+
+      <div class="buttons has-addons is-centered are-small ">
+        {#each [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] as n}
+          <button class="button is-primary is-outlined">{n}</button>
         {/each}
       </div>
 
@@ -37,12 +47,25 @@
       <button class="button">Continue Later</button>
     </div>
   </div>
-  <button class="modal-close is-large" aria-label="close" on:click />
 </div>
 
 <style>
   /* Direct hack onto steps content */
   .steps.is-small.is-vertical .steps-content {
-    margin-top: -1.6rem;
+    margin-top: -1.3rem;
+  }
+
+  .rate-button {
+    height: 32px;
+    width: 32px;
+    border: 1px solid #77cdfe;
+    display: inline-block;
+    border-radius: 50%;
+    text-align: center;
+    margin: 2px;
+    margin-right: 4px;
+    color: #77cdfe;
+    font-size: 0.85em;
+    padding-top: 4px;
   }
 </style>
